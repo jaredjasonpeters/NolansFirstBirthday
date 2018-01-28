4513e5aa835fa90d4d918e71e6558bc14af777fd
Jared Peters <jjpeters@dlfna.com>
2018-01-28 09:43:04 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view enginie', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+//HELPERS
+//-----------------------------------//
+
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+
+//ROUTES
+//-----------------------------------//
+
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Nolan\'s first birthday'
+  });
+});
+
+
+//-----------------------------------//
+
+app.listen(port, () => {
+  console.log(`port open on ${port}`);
+});
